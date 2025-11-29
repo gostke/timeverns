c9ab33469674b3670b9652c216281dd5454b208c
gostek37 <gostek37@gmail.com>
2025-11-30 00:25:59 +0100

    bomb anim

--- items/bomb.gd
+++ items/bomb.gd
@@ -2,10 +2,12 @@ extends Item
 class_name Bomb
 
 @onready var fuze: Timer = $Fuze
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func on_pick_up() -> void:
 	if fuze.is_stopped():
 		fuze.start()
+		animation_player.play("lit")
 	
 func on_put_down() -> void:
 	pass
