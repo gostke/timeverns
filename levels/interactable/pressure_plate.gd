6a78f1d34ceb95ea193571a5be4da3f0dc29ac9d
Łapciuch <andrew.f@interia.eu>
2025-11-30 00:49:19 +0100

    pressure plate

--- levels/interactable/pressure_plate.gd
+++ levels/interactable/pressure_plate.gd
@@ -1,7 +1,14 @@
 extends Switch
 
 func _physics_process(_delta: float) -> void:
+	
 	if $Area2D.get_overlapping_bodies().is_empty():
 		is_pressed = false
 	else:
 		is_pressed = true
+		
+	switch_sprites()
+
+func switch_sprites() -> void:
+	$Unpressed.visible = not is_pressed
+	$Pressed.visible = is_pressed
