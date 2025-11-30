eecbb24ecec3726e79d9aa62bb4a1ddd76ef3b94
Łapciuch <andrew.f@interia.eu>
2025-11-30 03:04:34 +0100

    tutorial level

--- levels/interactable/portal.gd
+++ levels/interactable/portal.gd
@@ -2,7 +2,15 @@ extends Area2D
 
 @export var is_open: bool = false
 @export var next_level_id:int 
+
+func _ready() -> void:
+	$AnimatedSprite2D.play("closed")
 	
+func _physics_process(_delta: float) -> void:
+	
+	if is_open:
+		$AnimatedSprite2D.play("opened")
+		
 func _on_body_entered(_body: Node2D) -> void:
 	
 	if is_open:
